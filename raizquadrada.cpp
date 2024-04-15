d0624c15edc25104a25c784ce27e3ef090990c76
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-15 22:54:04 +0000

    feito

---
+++ raizquadrada.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+int main(){
+
+    double x = 2 , y = 4;
+    cout << fixed << setprecision(2) << "A raiz quadrade de x + y é :"<<
+    sqrt(x + y ) << endl;
+
+
+
+    return 0 ;
+}
