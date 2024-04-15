e1b36e1e8c9075fde9bf42af804750daf70b8234
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-15 23:55:09 +0000

    feito

---
+++ leituraElementoArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    int arr[3];
+
+    printf("Digite 3 números inteiro:\n");
+    scanf("%d %d %d", &arr[0],&arr[1],&arr[2]);
+    printf("Você digitou: %d, %d, %d\n", arr[0],arr[1],arr[2]);
+    
+    return 0;
+}
