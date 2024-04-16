9793f93a6d825b5f3d03834c0075fcc86c05cc3c
FelipeJansen <132519887+felipeGJsilva@users.noreply.github.com>
2024-04-16 00:47:36 +0000

    feito

---
+++ exercicio2Array.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    int array[3] = {1,2,3};
+
+    int indice;
+
+    printf("Digite umn valor de um indice de 0 a 2: ");
+
+    scanf("%d",&indice);
+
+    printf("Elemento escolhido é: %d ", array[indice]);
+    
+
+    return 0;
+}
---
+++ exercicioArray.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+
+    int arr[5];
+
+    printf("Digite 5 números inteiro:\n");
+    scanf("%d %d %d %d %d %d", &arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5]);
+    printf("Você digitou: %d, %d, %d, %d, %d\n", arr[0],arr[1],arr[2],arr[3],arr[4]);
+    
+    return 0;
+}
